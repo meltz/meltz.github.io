6fc42d8b5f998e3cf126a1fcc2b93c0ea661e269
meltz <meltz@gmail.com>
2020-03-01 01:26:21 +0800

    CSS Styling done

--- includes/scripts.js
+++ includes/scripts.js
@@ -33,13 +33,13 @@ Vue.component('pagination', {
     },
     computed: {
         getPreviousID() {
-            return this.previousID = this.project + 1
+            return this.previousID = this.project - 1
         },
         getPreviousSlug() {
             return this.previousSlug = this.myPortfolio.filter(project => project.id === this.previousID).map(filteredObj => filteredObj.slug)
         },
         getNextID() {
-            return this.nextID = this.project - 1
+            return this.nextID = this.project + 1
         },
         getNextSlug() {
             return this.nextSlug = this.myPortfolio.filter(project => project.id === this.nextID).map(filteredObj => filteredObj.slug)
@@ -50,16 +50,22 @@ Vue.component('pagination', {
     },
     template:
     `<div class="project-pagination">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
 
-        <div class="previous-btn">
-            <span v-if="this.getPreviousID <= this.getTotalProject"><router-link :to="'/project/' + this.getPreviousSlug">Previous</router-link></span>
-            <span v-else>No Previous</span>
-        </div>
-        <div class="next-btn">
-            <span v-if="this.getNextID >= 1"><router-link :to="'/project/' + this.getNextSlug">Next</router-link></span>
-            <span v-else>No Next</span>
-        </div>
+                    <div class="previous-btn">
+                        <span v-if="this.getPreviousID >= 1"><router-link :to="'/project/' + this.getPreviousSlug">Previous</router-link></span>
+                        <span v-else>No Previous</span>
+                    </div>
+                    <div class="next-btn">
+                        <span v-if="this.getNextID <= this.getTotalProject"><router-link :to="'/project/' + this.getNextSlug">Next</router-link></span>
+                        <span v-else>No Next</span>
+                    </div>
 
+                </div>
+            </div>
+        </div>
 
     </div><!-- project-pagination -->`
 })
@@ -73,17 +79,16 @@ const Home = {
     template:
     `<div id="portfolio-section">
         <div class="container-fluid">
-            <div class="row">
-                <div class="col-12 col-md-3" v-for="project in latestProjects" :key="project.id">
-                    <div :id="'p' + project.id" class="project">
-                        <router-link :to="'/project/' + project.slug">
-                            <p>{{ project.id }}</p>
-                            <img :src="'/' + portfolioFolder + '/' + project.screen_name + '/' + project.screen_name + '-thumb.jpg'" :alt="project.name" class="img-fluid">
-                            <h3><strong>{{ project.name }}</strong></h3>
+            <div class="row justify-content-center">
+                <div v-for="project in latestProjects" :key="project.id" class="col-12 col-md-auto">
+                    <router-link :to="'/project/' + project.slug" :id="'p-' + project.id" class="project">
+                        <img :src="'/' + portfolioFolder + '/' + project.screen_name + '/' + project.screen_name + '-thumb.jpg'" :alt="project.name" class="img-fluid thumbnail">
+                        <div class="portfolio-content">
+                            <h3>{{ project.name }}</h3>
                             <p class="task">{{ project.task }}</p>
                             <p class="skills">{{ project.skills }}</p>
-                        </router-link>
-                    </div>
+                        </div>
+                    </router-link>
                 </div>
             </div>
         </div>
@@ -97,32 +102,49 @@ const Project = {
         },
     },
     template:
-    `<div id="portfolio-project">
-        <div class="container">
-            <div v-if="filterProject && filterProject.length">
-                <div v-for="project in filterProject">
-                    <pagination :project="project.id"></pagination>
-                    <div class="project-detail">
-                        <h3 class="name">{{ project.id + ' ' + project.name }}</h3>
-                        <div v-if="project.desc" class="desc" v-html="project.desc"></div>
-                        <p class="task">{{ project.task }}</p>
-                        <p class="skills">{{ project.skills }}</p>
-                        <a v-if="project.link" :href="project.link" target="_blank" class="link"><span class="link-text">Visit: {{ project.name }}</span></a>
-                    </div><!--.project-detail-->
-                    <div class="project-screens">
-                        <div v-for="screen in project.screen_total">
-                            <img :src="'/' + portfolioFolder + '/' + project.screen_name + '/' + project.screen_name + '-' + screen + '.jpg'" :alt="project.name" class="img-fluid">
+    `<div id="project-section">
+        <div v-if="filterProject && filterProject.length">
+            <div v-for="project in filterProject">
+
+                <pagination :project="project.id"></pagination>
+
+                <div class="project-detail-section">
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-8">
+                                <div class="project-detail">
+                                    <h3 class="name">{{ project.name }}</h3>
+                                    <div v-if="project.desc" class="desc" v-html="project.desc"></div>
+                                    <p class="task">{{ project.task }}</p>
+                                    <p class="skills">{{ project.skills }}</p>
+                                    <a v-if="project.link" :href="project.link" target="_blank" class="link"><span class="link-text">Click to view</span></a>
+                                </div><!-- .project-detail -->
+                            </div>
                         </div>
-                    </div><!--.project-screens-->
-                    <pagination :project="project.id"></pagination>
-                </div>
-            </div>
-            <div v-else>
-                <p>404 Page not found</p>
-                <p><router-link to="/">Back</router-link></p>
+                    </div>
+                </div><!-- .project-detail-section -->
+
+                <div class="project-screens-section">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-12">
+                                <div v-for="screen in project.screen_total" class="screenshot">
+                                    <img :src="'/' + portfolioFolder + '/' + project.screen_name + '/' + project.screen_name + '-' + screen + '.jpg'" :alt="project.name" class="img-fluid">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div><!-- .project-screens-section -->
+
+                <pagination :project="project.id"></pagination>
+
             </div>
         </div>
-    </div><!-- #portfolio-project -->`,
+        <div v-else>
+            <p>404 Page not found</p>
+            <p><router-link to="/">Back</router-link></p>
+        </div>
+    </div><!-- #project-section -->`,
 }
 
 const router = new VueRouter ({
@@ -151,4 +173,10 @@ const router = new VueRouter ({
 
 const app = new Vue ({
     router,
+    methods: {
+        getYear() {
+            const date = new Date()
+            return date.getFullYear()
+        }
+    }
 }).$mount('#app')
